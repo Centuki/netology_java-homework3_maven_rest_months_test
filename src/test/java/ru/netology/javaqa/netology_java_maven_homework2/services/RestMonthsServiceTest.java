e4d6b9fc0d39fc8b793ca5fe4ef1b0ab2f136c43
Ruslan Somov <rusl.somow@gmail.com>
2025-07-21 12:48:52 +0300

    Parameterized Test added

--- src/test/java/ru/netology/javaqa/netology_java_maven_homework2/services/RestMonthsServiceTest.java
+++ src/test/java/ru/netology/javaqa/netology_java_maven_homework2/services/RestMonthsServiceTest.java
@@ -1,30 +1,17 @@
 package ru.netology.javaqa.netology_java_maven_homework2.services;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RestMonthsServiceTest {
-    @Test
-    public void RestMonthsServiceTestFirst() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "incomeExpensesThreshold.csv")
+    public void RestMonthsServiceTestFirst(int expected, int income, int expenses, int threshold) {
         RestMonthsService service = new RestMonthsService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
         int actual = service.calculate(income, expenses, threshold);
-        int expected = 3;
-
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void RestMonthsServiceTestSecond() {
-        RestMonthsService service = new RestMonthsService();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int actual = service.calculate(income, expenses, threshold);
-        int expected = 2;
-
-        Assertions.assertEquals(expected, actual);
-    }
 }
